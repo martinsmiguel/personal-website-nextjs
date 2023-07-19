8efa36c42970d01ea1fbadf56d63ddf57b5ff982
Miguel Martins <miguelrjmartins9@gmail.com>
2023-07-19 19:35:31 -0400

    feat: changes to about page and resume page

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -2,7 +2,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blog' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projetos' },
-  { href: '/resume', title: 'Currículo' },
+  // { href: '/resume', title: 'Currículo' },
   { href: '/about', title: 'Sobre' },
 ]
 
